ee9e4cfecb883ff38c13ad707278a661e1b8add6
lucode <yunfeng.lu@Bangdao-tech.com>
2017-10-03 16:44:17 +0800

    StringUtils、HttpClient

---
+++ fast-dev-util/src/main/java/cn/lucode/util/httpclient/HttpCookies.java
@@ -0,0 +1,54 @@
+package cn.lucode.util.httpclient;
+
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+
+/**
+ * Created by yunfeng.lu on 2017/10/3.
+ */
+
+
+
+public class HttpCookies {
+
+    /**
+     * 使用httpcontext，用于设置和携带Cookie
+     */
+    private HttpClientContext context ;
+
+    /**
+     * 储存Cookie
+     */
+    private CookieStore cookieStore;
+
+    public static HttpCookies custom(){
+        return new HttpCookies();
+    }
+
+    private HttpCookies(){
+        this.context = new HttpClientContext();
+        this.cookieStore = new BasicCookieStore();
+        this.context.setCookieStore(cookieStore);
+    }
+
+    public HttpClientContext getContext() {
+        return context;
+    }
+
+    public HttpCookies setContext(HttpClientContext context) {
+        this.context = context;
+        return this;
+    }
+
+    public CookieStore getCookieStore() {
+        return cookieStore;
+    }
+
+    public HttpCookies setCookieStore(CookieStore cookieStore) {
+        this.cookieStore = cookieStore;
+        return this;
+    }
+
+
+}
